e56190b2847f263fa4c99523b303da5a02610f4b
BuildTools <unconfigured@null.spigotmc.org>
2022-03-19 13:17:46 -0700

    initial project

---
+++ TestGame-GL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TestGame_GL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
